25d8b34451e6a4bd263f8675abb5a3c396ad4f2e
Parveen Singh <42755757+singhparveen@users.noreply.github.com>
2020-06-27 19:08:34 -0700

    Update install_apps.ps1

--- install_apps.ps1
+++ install_apps.ps1
@@ -1,8 +1,3 @@
 $ErrorActionPreference="Stop"
 #VSCode
 Start-Process -FilePath "Z:\VSCode\VSCodex64-1.46.1.exe" -ArgumentList "/VERYSILENT /NORESTART /MERGETASKS=!runcode" -Wait -NoNewWindow
-$appName = "VS Code 1.46.1"
-$installObject = Get-WmiObject -Class Win32_Product | Where-Object Name -Match "$appName"
-if ($installObject) {
-  Write-Host "$appName is installed."
-}
